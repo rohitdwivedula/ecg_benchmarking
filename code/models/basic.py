88bc6b65fa35d144c39fab588a780fbf2c389abf
Rohit Dwivedula <rohitdwivedula@gmail.com>
2020-11-22 22:26:00 +0530

    Add kernel regularization and reduce lr

--- code/models/basic.py
+++ code/models/basic.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 from keras.models import Model
 from keras.layers import Attention, Dense, Dropout, LSTM, Input, Conv1D, Bidirectional, GRU, Flatten, Reshape
 from keras import backend as K
+from keras.regularizers import l2
+from keras.optimizers import Adam
 
 # GPU
 from tensorflow.compat.v1 import ConfigProto
@@ -16,21 +18,21 @@ def stacked_lstm(units=[128, 128], lstm_activation = 'tanh', num_output_classes 
     inp = Input(shape=(12, 1000))
     
     if len(units) == 1:
-        x = LSTM(units[0], activation = lstm_activation)
+        x = LSTM(units[0], activation = lstm_activation, kernel_regularizer=l2(0.01), recurrent_regularizer=l2(0.01), bias_regularizer=l2(0.01))
     else:
-        x = LSTM(units[0], activation = lstm_activation, return_sequences = True)
+        x = LSTM(units[0], activation = lstm_activation, return_sequences = True, kernel_regularizer=l2(0.01), recurrent_regularizer=l2(0.01), bias_regularizer=l2(0.01))
 
     for i in range(1, len(units)):
         if i != len(units) - 1:
-            x = LSTM(units[i], activation = lstm_activation, return_sequences = True)(inp)
+            x = LSTM(units[i], activation = lstm_activation, return_sequences = True, kernel_regularizer=l2(0.01), recurrent_regularizer=l2(0.01), bias_regularizer=l2(0.01))(inp)
         else:
-            x = LSTM(units[i], activation = lstm_activation)(inp)
+            x = LSTM(units[i], activation = lstm_activation, kernel_regularizer=l2(0.01), recurrent_regularizer=l2(0.01), bias_regularizer=l2(0.01))(inp)
 
     if units[-1] > 128 and add_dense:
-        x = Dense(128, activation='relu')(x)
+        x = Dense(128, activation='relu', kernel_regularizer=l2(0.001))(x)
 
     if add_dense and num_output_classes < 64:
-        x = Dense(64, activation='relu')(x)
+        x = Dense(64, activation='relu', kernel_regularizer=l2(0.001))(x)
 
     out = Dense(num_output_classes, activation='softmax')(x)
     model = Model(inp, out)
@@ -67,7 +69,8 @@ class BasicModel(ClassificationModel):
         else:
         	print("Model", self.name, "not found")
         	exit(0)
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        adam = Adam(lr=1e-4)
+        model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
         print("[MODEL] Array Sizes")
         print("[MODEL] X_train", X_train.shape)
         print("[MODEL] y_train", y_train.shape)
